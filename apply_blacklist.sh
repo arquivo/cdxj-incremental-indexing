1f3701a78394bc382fcf22a59551f7edb16ec49e
Francisco Esteveira <festeveira@fccn.pt>
2023-11-16 18:43:19 +0000

    fix

--- apply_blacklist.sh
+++ apply_blacklist.sh
@@ -12,11 +12,11 @@ else
     workdir="$(dirname "$CDXJ_OUTPUT_FILE")/blacklist_tmp"
     mkdir -p "$workdir"
     cd "$workdir"
-    
+
     split -d -n l/"${PARALLEL_N}"  "$CDXJ_INPUT_FILE" "$workdir/part.$(basename "$CDXJ_INPUT_FILE")"
     ls "part.$(basename "$CDXJ_INPUT_FILE")"* | xargs -P ${PARALLEL_N} -I file sh -c "grep -a -E -v -f ${BLACKLIST_PATTERNS_FILE} file > filtered.file"
     rm "part.$(basename ${CDXJ_INPUT_FILE})"*
-    cat "filtered.part.$(basename ${CDXJ_INPUT_FILE})"* > "${CDXJ_OUTPUT_FILE}"´
+    cat "filtered.part.$(basename ${CDXJ_INPUT_FILE})"* > "${CDXJ_OUTPUT_FILE}"
 
     cd $OLDPWD
     rm -rf $workdir
