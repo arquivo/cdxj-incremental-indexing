aac7ec91967a55faf85425212a7614d557181286
Francisco Esteveira <festeveira@fccn.pt>
2023-11-16 18:38:12 +0000

    fix

--- apply_blacklist.sh
+++ apply_blacklist.sh
@@ -6,13 +6,13 @@ CDXJ_OUTPUT_FILE="$2"
 BLACKLIST_PATTERNS_FILE="$3"
 PARALLEL_N="$4"
 
-workdir="$(dirname "$CDXJ_OUTPUT_FILE")/blacklist_tmp"
-mkdir -p "$workdir"
-cd "$workdir"
-
 if [ -z "${PARALLEL_N}" ]; then
     grep -a -E -v -f ${BLACKLIST_PATTERNS_FILE} "${CDXJ_INPUT_FILE}" > "${CDXJ_OUTPUT_FILE}"
 else
+    workdir="$(dirname "$CDXJ_OUTPUT_FILE")/blacklist_tmp"
+    mkdir -p "$workdir"
+    cd "$workdir"
+    
     split -d -n l/"${PARALLEL_N}"  "$CDXJ_INPUT_FILE" "$workdir/part.$(basename "$CDXJ_INPUT_FILE")"
     ls "part.$(basename "$CDXJ_INPUT_FILE")"* | xargs -P ${PARALLEL_N} -I file sh -c "grep -a -E -v -f ${BLACKLIST_PATTERNS_FILE} file > filtered.file"
     rm "part.$(basename ${CDXJ_INPUT_FILE})"*
